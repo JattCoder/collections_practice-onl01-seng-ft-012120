37b06780b6ff3a0f992d40a7d3b6e25fa38d4f20
Harmandeep Mand <harmandeepmand.hm@gmail.com>
2020-01-26 00:24:52 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,35 @@
+def sort_array_asc (arr)
+  return arr.sort
+end
 
+def sort_array_desc (arr)
+  return arr.sort.reverse
+end
+
+def sort_array_char_count (arr)
+  return arr.sort_by(&:length)
+end
+
+def swap_elements (arr,index,destination_index)
+
+end
+
+def reverse_array (arr)
+
+end
+
+def kesha_maker (arr)
+
+end
+
+def find_a (arr)
+
+end
+
+def sum_array (arr)
+
+end
+
+def add_s (arr)
+
+end
